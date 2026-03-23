df4f335178626409952abc5c97fa0c1d4b18b4fa
Chandan Singh <240205019+Chandantbh@users.noreply.github.com>
2026-03-23 21:03:40 +0530

    Initial commit, LFS fixed

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Motivated and enthusiastic beginner web developer with a strong interest in building modern websites and learning new technologies. Currently exploring Git, GitHub, and full-stack development with a focus on creating real-world projects. Quick learner and consistent problem solver, pursuing studies in Engineering.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
